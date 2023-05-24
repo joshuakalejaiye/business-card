20adf7876967792058dfaabdebb3b5e27bdf513d
Adedayo (Joshua) Kalejaiye <32837010+joshuakalejaiye@users.noreply.github.com>
2023-05-24 11:18:54 +0100

    responsive actually this time

--- src/routes/Details.svelte
+++ src/routes/Details.svelte
@@ -4,7 +4,7 @@
 
 <div
 	id="details"
-	class="font-spacemono w-200 text-[#744F00] p-[100px] drop-shadow-2px-2px-2px-212121 bg-[#FECE66]"
+	class="font-spacemono w-200 text-[#744F00] p-6 sm:p-[100px] drop-shadow-2px-2px-2px-212121 bg-[#FECE66]"
 >
 	<div id="name" class="font-milligram text-5xl lg:text-6xl">Adedayo Kalejaiye (Joshua)</div>
 	<span id="role-container" class="flex justify-center lg:justify-normal gap-3 mt-1 mb-1">
