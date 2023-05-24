102bdc7886027ca32076315b2c437bab17f7ac47
Adedayo (Joshua) Kalejaiye <32837010+joshuakalejaiye@users.noreply.github.com>
2023-05-24 10:00:06 +0100

    prettier but less optimised

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,15 @@
 </script>
 
 <div class="app">
-	<main>
+	<main
+		class="w-full h-full bg-[url('../lib/images/ebun_oluwole_max.webp')] bg-[center] bg-cover bg-image bg-blend-darken"
+	>
 		<slot />
 	</main>
 </div>
 
 <style>
 	:global(html) {
-	  background-color: #050918;
+		background-color: #bf8c1d;
 	}
 </style>
--- src/routes/Details.svelte
+++ src/routes/Details.svelte
@@ -2,21 +2,28 @@
 	import TypedRole from './TypedRole.svelte';
 </script>
 
-<div id="details" class="font-spacemono w-200 text-white p-6">
-    <div id="name" class="font-milligram text-5xl lg:text-6xl">Adedayo Kalejaiye (Joshua)</div>
-    <span id="role-container" class="flex justify-center lg:justify-normal gap-3 mt-1 mb-1">
-        <TypedRole />
-    </span>
-    <span id="links" class="flex justify-center lg:justify-normal gap-5">
-        <a href="http://github.com/joshuakalejaiye" target="_blank" id="gh" >GitHub</a>
-        <a href="http://linkedin.com/in/joshuakalejaiye" id="linkedin">LinkedIn</a>
-        <div id="blog" class="text-gray-500">Blog (soon...)</div>
-    </span>
+<div
+	id="details"
+	class="font-spacemono w-200 text-[#744F00] p-[100px] drop-shadow-2px-2px-2px-212121 bg-[#FECE66]"
+>
+	<div id="name" class="font-milligram text-5xl lg:text-6xl">Adedayo Kalejaiye (Joshua)</div>
+	<span id="role-container" class="flex justify-center lg:justify-normal gap-3 mt-1 mb-1">
+		<TypedRole />
+	</span>
+	<span id="links" class="flex justify-center lg:justify-normal gap-5">
+		<a href="http://github.com/joshuakalejaiye" class="hover:text-white" target="_blank" id="gh"
+			>GitHub</a
+		>
+		<a href="http://linkedin.com/in/joshuakalejaiye" class="hover:text-white" id="linkedin"
+			>LinkedIn</a
+		>
+		<div id="blog" class="text-[#BF8C1D]">Blog (soon...)</div>
+	</span>
 </div>
 
 <style>
-    * { 
+	* {
 		/* border: 2px solid red; */
-		text-align: center;	
-    }
+		text-align: center;
+	}
 </style>
