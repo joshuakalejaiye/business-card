0d395f8a4d8265eee9b49950066218e8fb1f9b2b
Adedayo (Joshua) Kalejaiye <32837010+joshuakalejaiye@users.noreply.github.com>
2023-05-24 16:01:05 +0100

    testing uploadthing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,27 @@
-<script>
+<script lang="ts">
 	import './app.css';
+
+	let waiting = 0;
+
+	const notifyLoaded = () => {
+		console.log('loaded!');
+	};
+
+	const onload = (el: { addEventListener: (arg0: string, arg1: () => void) => void }) => {
+		waiting++;
+		el.addEventListener('load', () => {
+			waiting--;
+			if (waiting === 0) {
+				notifyLoaded();
+			}
+		});
+	};
 </script>
 
 <div class="app">
 	<main
-		class="w-full h-full bg-[url('../lib/images/ebun_oluwole_max.webp')] bg-[center] bg-cover bg-image bg-blend-darken"
+		use:onload
+		class="main w-full h-full bg-[url('https://uploadthing.com/f/fa9be026-eed7-48ea-ad0c-8e2b3c2a21b5_ebun_oluwole_max_its_webp.jpg')] bg-[center] bg-cover bg-image bg-blend-darken"
 	>
 		<slot />
 	</main>
